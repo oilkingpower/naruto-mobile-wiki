3a4134d7f2e6d1129b0bfa708c1f1e2419254fe0
oilkingpower <wozhale2021@gmail.com>
2025-09-15 18:55:01 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="zh">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>火影忍者wiki</title>
-</head>
-<body>
-    <div id="app"></div>
-    <script type="module" src="/src/main.tsx"></script>
-</body>
-</html>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('app')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
